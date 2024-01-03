0a100545e1d124846345b4231e76b2ade1134477
ThallesRibeiro <127794820+ThallesRibeiroNeves@users.noreply.github.com>
2024-01-03 18:32:06 -0300

    criação da aula 008

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,9 @@
-console.log('O console funcionou corretamente!')
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel >60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+else {
+    console.log(`Você está dentro do limite de velocidade, parabéns!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
