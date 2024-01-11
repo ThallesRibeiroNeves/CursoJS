56aeed23300824a363f7fb807b6fd2b445f487c4
ThallesRibeiro <127794820+ThallesRibeiroNeves@users.noreply.github.com>
2024-01-10 21:04:58 -0300

    criação da aula 12

---
+++ aula12/ex12.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12 && hora > 5){
+    console.log ('Bom dia!')
+} else if (hora > 11 && hora <= 18){
+    console.log('Boa tarde!')
+} else if (hora < 6){
+    console.log('Boa madrugada!')
+
+} else {
+    console.log('Boa noite!')
+}
