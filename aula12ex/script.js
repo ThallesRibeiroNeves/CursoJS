23181f08b03f78cbd0b7faba9f94445c0b91f40f
ThallesRibeiro <127794820+ThallesRibeiroNeves@users.noreply.github.com>
2024-01-17 20:18:26 -0300

    add aula 12exercicio

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12){
+    img.src = 'manha.png'
+    document.body.style.background = '#9F8E62'
+}   else if (hora >= 12 && hora <18){
+    img.src = 'tarde.png'
+    document.body.style.background = '#8B5534'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#A57398'
+
+    }
+}
