5f213010a2e2453b3dc7d753ed2436044ef430a5
ThallesRibeiro <127794820+ThallesRibeiroNeves@users.noreply.github.com>
2024-02-15 20:17:42 -0300

    criação desafio17

---
+++ aula17/desafio17/script.js
@@ -0,0 +1,19 @@
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert ('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        var res = n*c
+        tab.innerHTML = ''
+        for (var c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${res}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+
+    }
+}
