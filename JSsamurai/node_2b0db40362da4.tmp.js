bc1fd46d1bd2b4b9badf73945eb2a5faf38460cc
ThallesRibeiro <127794820+ThallesRibeiroNeves@users.noreply.github.com>
2024-03-29 05:53:59 -0300

    atts

---
+++ JSsamurai/node_2b0db40362da4.tmp.js
@@ -0,0 +1,7 @@
+var contador = 1
+var limite = 10
+    while (contador <= limite){
+        console.log (contador + 'Test')
+        contador = contador + 1
+    }
+    console.log ('Fim da execução!')
